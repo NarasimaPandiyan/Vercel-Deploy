b77092f084796d55243506562dd64f5cab1dc587
Narasima Pandiyan <35830653+NarasimaPandiyan@users.noreply.github.com>
2023-04-26 21:54:01 +0530

    mono-file

--- api.py
+++ api.py
@@ -1,8 +1,36 @@
 import os
 from fastapi import FastAPI
-from mongoDB import ping, get_messages, post_message
 
 app = FastAPI()
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+
+load_dotenv()
+
+uri = os.getenv("MONGO_URI")
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+db = client.CalmCove
+
+def post_message(chat: str, message: str):
+    pass
+
+def get_messages(chat: str):
+    cursor = db[chat].find({},{'_id': False})
+    data = []
+    for doc in cursor:
+        data.append(doc)
+    return data
+
+def ping():
+    try:
+        client.admin.command('ping')
+        return {f"Pinged your deployment. You successfully connected to MongoDB! {db.list_collection_names()}"}
+    except Exception as e:
+        return({"error":e})
 
 @app.get("/test/{chat}")
 def hello(chat: str):
